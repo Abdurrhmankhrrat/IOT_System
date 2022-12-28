357468f84d79b15d67f21408d27bee9125b13ecc
Abdurrhmankhrrat <42301187+Abdurrhmankhrrat@users.noreply.github.com>
2022-12-28 21:35:11 +0300

    Update BottomBar1.dart

--- lib/componant/BottomBar1.dart
+++ lib/componant/BottomBar1.dart
@@ -1,5 +1,6 @@
 // ignore: file_names
 // ignore_for_file: file_names
+//Abdurrhmankhrrat
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:iot_switch/controllar/gps.dart';
